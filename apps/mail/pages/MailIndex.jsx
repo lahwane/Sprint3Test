e21bcfc2e0480ed7366532c76c38835ffa72f949
lahwane <lahwaneasaad@gmail.com>
2025-02-23 17:09:37 +0200

    Add files via upload

--- apps/mail/pages/MailIndex.jsx
+++ apps/mail/pages/MailIndex.jsx
@@ -1,5 +1,39 @@
 
+const { useEffect, useState } = React
+import { mailService } from "../services/mail.service.js"
+
 export function MailIndex() {
-    return <section className="container">Mail app</section>
+
+    const [mails, setMails] = useState(null)
+    const [filterBy, setFilterBy] = useState(mailService.getDefaultFilter())
+
+    useEffect(()=>{
+        loadMails()
+    },[filterBy])
+
+    function loadMails(){
+        mailService.query(filterBy)
+        .then(mails=>{
+            setMails(mails)
+        })
+        .catch(err=>{
+            console.error('Error in loading mails:',err)
+        })
+    }
+
+    function onSetFilterBy(filterBy){
+        setFilterBy({...filterBy})
+    }
+
+
+
+
+    return (
+    <section className="mail-index-container">
+        {console.log(mails)}
+        <pre>{JSON.stringify(mails, null, 2)}</pre>
+    </section>)
 }
+
+
 
